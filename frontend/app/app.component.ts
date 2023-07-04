af9bd3c5f20a1fab8b6a3152fb37ba593d6577f7
Nathn <nathan@tranchant.fr>
2023-07-04 22:16:07 +0200

    Add Firebase Auth module + Angular env

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,12 +16,12 @@ export class AppComponent {
   title = 'MasterQuizz';
   backendUrl: string = `http://${window.location.hostname}:3000/`;
   ping: string = '';
-  got_title: boolean = false;
 
   constructor(
     private router: Router,
     private http: HttpClient
   ) {
+    initializeApp(environment.firebaseConfig);
     this.pingServer();
   }
 
