43b0eb41592e516521818497aacf143c89c9a4ca
Nathn <nathan@tranchant.fr>
2023-07-10 11:44:43 +0200

    Add Question's backend

---
+++ backend/models/Question.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
+const questionSchema = new mongoose.Schema({
+    answers: [{
+        answer: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        correct: {
+            type: Boolean,
+            required: true
+        }
+    }],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    question: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    theme: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: false
+    }
+});
+
+questionSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('Question', questionSchema);
---
+++ backend/routes/questionRoutes.js
@@ -0,0 +1,101 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Question = require('../models/Question');
+const User = require('../models/User');
+
+router.post('/createQuestion', async (req, res) => {
+    try {
+        console.log(`[SERVER] Creating a new question: ${req.body.question}`);
+        question = new Question({
+            answers: req.body.answers,
+            difficulty: req.body.difficulty,
+            question: req.body.question,
+            theme: req.body.theme,
+            user: req.body.user
+        });
+        await question.save();
+        // And return a success message
+        res.status(200).json({
+            message: 'OK'
+        });
+    } catch (err) {
+        console.log(`[SERVER] An error occured while creating a new question: ${err}`);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+});
+
+router.delete('/deleteQuestion', async (req, res) => {
+    try {
+        console.log(`[SERVER] Deleting a question: ${req.body.questionId}`);
+        await Question.deleteOne({
+            _id: req.body.questionId
+        }).exec()
+            .then(question => {
+                if (!question) {
+                    console.log(`[SERVER] Question not found while deleting a question`);
+                    res.status(200).json({
+                        message: 'Question introuvable.'
+                    });
+                } else {
+                    console.log(`[SERVER] Question deleted: ${question._id}`);
+                    res.status(200).json({
+                        message: 'OK'
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(`[SERVER] An error occured while deleting a question: ${err}`);
+                res.status(500).json({
+                    message: 'Internal server error'
+                });
+            });
+    } catch (err) {
+        console.log(`[SERVER] An error occured while deleting a question: ${err}`);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+});
+
+router.post('/getRandomQuestion', async (req, res) => {
+    try {
+        console.log(`[SERVER] Getting a random question`);
+        await Question.aggregate([{
+            $sample: {
+                size: 1
+            }
+        }]).exec()
+            .then(question => {
+                if (!question) {
+                    console.log(`[SERVER] Question not found while getting a random question`);
+                    res.status(200).json({
+                        message: 'Question introuvable.'
+                    });
+                } else {
+                    console.log(`[SERVER] Question found: ${question._id}`);
+                    res.status(200).json({
+                        message: 'OK',
+                        question: question
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(`[SERVER] An error occured while getting a random question: ${err}`);
+                res.status(500).json({
+                    message: 'Internal server error'
+                });
+            });
+    } catch (err) {
+        console.log(`[SERVER] An error occured while getting a random question: ${err}`);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+});
+
+
+module.exports = router;
