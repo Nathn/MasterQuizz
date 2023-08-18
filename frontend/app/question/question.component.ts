01803ae833dbb719eb9af559ee1684b36ec52638
Nathn <nathan@tranchant.fr>
2023-08-18 12:13:26 +0200

    Add stars to show difficulty

--- frontend/app/question/question.component.ts
+++ frontend/app/question/question.component.ts
@@ -1,33 +1,38 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { faSpinner, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {
+    faSpinner,
+    faCheck,
+    faTimes,
+    faStar,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+    selector: 'app-question',
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent {
-  @Input('question') question: any;
-  @Input('answerValidated') answerValidated: boolean = false;
-  @Input('selectedAnswerIndex') selectedAnswerIndex: number = -1;
-  @Input('questionOptions') questionOptions: any = {} as any;
+    @Input('question') question: any;
+    @Input('answerValidated') answerValidated: boolean = false;
+    @Input('selectedAnswerIndex') selectedAnswerIndex: number = -1;
+    @Input('questionOptions') questionOptions: any = {} as any;
 
-  @Output() selectedAnswer = new EventEmitter();
+    @Output() selectedAnswer = new EventEmitter();
 
-  faSpinner = faSpinner;
-  faCheck = faCheck;
-  faTimes = faTimes;
+    faSpinner = faSpinner;
+    faCheck = faCheck;
+    faTimes = faTimes;
+    faStar = faStar;
 
-  constructor() { }
+    constructor() {}
 
-  selectAnswer(index: number) {
-    if (this.selectedAnswerIndex == index) {
-      this.selectedAnswerIndex = -1;
-    } else {
-      this.selectedAnswerIndex = index;
+    selectAnswer(index: number) {
+        if (this.selectedAnswerIndex == index) {
+            this.selectedAnswerIndex = -1;
+        } else {
+            this.selectedAnswerIndex = index;
+        }
+        this.selectedAnswer.emit(this.selectedAnswerIndex);
     }
-    this.selectedAnswer.emit(this.selectedAnswerIndex);
-  }
-
 }
