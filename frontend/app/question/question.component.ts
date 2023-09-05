e4b68c01ec5eb356463a974828b4cf9b0d19dac5
Nathn <nathan@tranchant.fr>
2023-09-05 20:28:56 +0200

    Randomly shuffle the answers

--- frontend/app/question/question.component.ts
+++ frontend/app/question/question.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnInit,
+    SimpleChanges
+} from '@angular/core';
 
 @Component({
     selector: 'app-question',
     templateUrl: './question.component.html',
-    styleUrls: ['./question.component.scss'],
+    styleUrls: ['./question.component.scss']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
     @Input('question') question: any;
     @Input('answerValidated') answerValidated: boolean = false;
     @Input('selectedAnswerIndex') selectedAnswerIndex: number = -1;
@@ -13,7 +20,37 @@ export class QuestionComponent {
 
     @Output() selectedAnswer = new EventEmitter();
 
+    shuffledAnswers: any[] = [];
+
     constructor() {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        // if queestions is changed, shuffle the answers
+        if (changes['question']) {
+            this.shuffledAnswers = this.shuffleArray(
+                this.question.answers.slice()
+            );
+        }
+    }
+
+    ngOnInit() {
+        // Create a shuffled copy of the answers array
+        this.shuffledAnswers = this.shuffleArray(this.question.answers.slice());
+    }
+
+    shuffleArray(array: any[]) {
+        // Shuffle the array using the Fisher-Yates algorithm
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    getOriginalIndex(index: number) {
+        // Get the original index of the answer in the answers array
+        return this.question.answers.indexOf(this.shuffledAnswers[index]);
+    }
 
     selectAnswer(index: number) {
         if (this.selectedAnswerIndex == index) {
