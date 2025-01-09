c31e321b70147bad55081326e17f6bbf04c9fb23
Nathn <nathan@tranchant.fr>
2025-01-09 10:42:49 +0000

    Base environment file

---
+++ frontend/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: `http://${window.location.hostname}:3000/`,
+    wsUrl: `ws://${window.location.hostname}:3000`,
+    cookieDomain: window.location.hostname,
+    firebaseConfig: {
+    },
+    encryptionKey: 'example'
+};
